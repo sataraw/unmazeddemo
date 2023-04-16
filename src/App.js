2149cd9b8008e03f2c35a5e8fcad19eb7f4b82d9
Seat <awar.satar@outlook.de>
2023-04-16 14:46:12 +0200

    firebase

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import firebase  from 'firebase/compat/app'
+import 'firebase/compat/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC4y1MM8bx8Qi2qGWDYRXzCClJkyK-e7Ys",
+  authDomain: "unmazeddemo.firebaseapp.com",
+  projectId: "unmazeddemo",
+  storageBucket: "unmazeddemo.appspot.com",
+  messagingSenderId: "73326861454",
+  appId: "1:73326861454:web:cc15c4c9944b7c9793fddf",
+  measurementId: "G-EFTE1QCKTP"
+};
+
+firebase.initializeApp(firebaseConfig)
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>HELLO</h1>
       </header>
     </div>
   );

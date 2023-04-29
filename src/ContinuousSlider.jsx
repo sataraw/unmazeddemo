484e5afa1268bd0dcfb54bdefa9fbb956d0361fb
Seat <awar.satar@outlook.de>
2023-04-29 17:33:56 +0200

    Full Player functionality, Slider, Return button

--- src/ContinuousSlider.jsx
+++ src/ContinuousSlider.jsx
@@ -5,67 +5,117 @@ import Add from "./svg/Add.svg";
 import Remove from "./svg/Remove.svg";
 import Return from "./svg/returnButton.svg";
 import Slider from "@mui/material/Slider";
-import { styled } from '@mui/material/styles'; 
-import IconButton from '@mui/material/IconButton';
+import { styled } from "@mui/material/styles";
+import IconButton from "@mui/material/IconButton";
 import Player from "./Player";
+import { Tooltip } from "@mui/material";
 
 const PrettoSlider = styled(Slider)({
-  color: '#ff8a3b',
+  color: "#ff8a3b",
   height: 250,
   width: 6,
-  boxShadow: '0 0 0 10',
-  '& .MuiSlider-track': {
-    border: 'none',
+  boxShadow: "0 0 0 10",
+  "& .MuiSlider-track": {
+    border: "none",
   },
-  '& .MuiSlider-thumb': {
+  "& .MuiSlider-thumb": {
     height: 24,
     width: 24,
-    backgroundColor: '#fff',
-    '&:focus, &:hover, &.Mui-active, &.Mui-focusVisible, &.Mui-disabled': {
-      boxShadow: '0 0 0 10',
+    backgroundColor: "#fff",
+    "&:focus, &:hover, &.Mui-active, &.Mui-focusVisible, &.Mui-disabled": {
+      boxShadow: "0 0 0 10",
+    },
+  },
+  '& .MuiSlider-valueLabel': {
+    lineHeight: 1.2,
+    fontSize: 16,
+    background: 'unset',
+    padding: 4 ,
+    fontWeight: 600,
+    width: 40,
+    height: 40,
+    borderRadius: '50% 50% 50% 0',
+    backgroundColor: '#ff8a3b',
+    transformOrigin: 'bottom left',
+    transform: 'translate(100%, -100%) rotate(-90deg) scale(0)',
+    '&:before': { display: 'none' },
+    '&.MuiSlider-valueLabelOpen': {
+      transform: 'translate(75%, -100%) rotate(-90deg) scale(1)',
+    },
+    '& > *': {
+      transform: 'rotate(90deg)',
     },
   },
 });
 const ReturnButton = styled(IconButton)({
-    color: '#ff8a3b',
-    height: 57,
-    width: 58,
-    boxShadow: '0 0 0 10',
-     backgroundColor: '#ff8a3b',
-    '&:focus, &:hover' : {
-      backgroundColor: '#ff8a3b',
-    },
-    '&:active': {
-      backgroundColor: '#CB5200',
-    }
-})
+  color: "#ff8a3b",
+  height: 57,
+  width: 58,
+  boxShadow: "0 0 0 10",
+  backgroundColor: "#ff8a3b",
+  "&:focus, &:hover": {
+    backgroundColor: "#ff8a3b",
+  },
+  "&:active": {
+    backgroundColor: "#CB5200",
+  },
+});
 
 export const SliderContext = React.createContext(50);
 
 function ContinuousSlider() {
-
-  const [value, setValue] = React.useState(50);
+  const [value, setValue] = React.useState(33);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
-  }
+  };
 
-    return (
-      <SliderContext.Provider value={value}>
-        <Player/>
-        <div className="SliderContainer">
-          <Box sx={{ height: 389, width: 36 }}>
-          <Stack  direction="column"  alignItems="center" justifyContent="flex-start"  className="SliderBox"> 
-            <img src={Add} id="Add" className="SliderIcon" alt='Add'/>
-            <PrettoSlider orientation="vertical" onChange={handleChange} value={value}/>
-            <img src={Remove} className="SliderIcon" alt='Remove'/>
-            <ReturnButton className="ReturnButton">
-              <img src={Return} alt='return'/>
+  const [returnClicked, setReturnClicked] = React.useState(false);
+
+  const handleReturnClicked = () => {
+    console.log("clicked Slider");
+    setReturnClicked(true);
+  };
+  const handleReturnReset = () => {
+    console.log("reset Slider");
+    setReturnClicked(false);
+  };
+
+  return (
+    <SliderContext.Provider value={value}>
+      <Player
+        returnClicked={returnClicked}
+        onReturnClicked={handleReturnClicked}
+        onReturnReset={handleReturnReset}
+      />
+      <div className="SliderContainer">
+        <Box sx={{ height: 389, width: 36 }}>
+          <Stack
+            direction="column"
+            alignItems="center"
+            justifyContent="flex-start"
+            className="SliderBox"
+          >
+            <img src={Add} id="Add" className="SliderIcon" alt="Add" />
+            <PrettoSlider
+              orientation="vertical"
+              onChange={handleChange}
+              value={value}
+              valueLabelDisplay="auto"
+              valueLabelFormat={(0.5 + (value / 100) * 1.5).toFixed(1) + 'x'}
+            />
+            <img src={Remove} className="SliderIcon" alt="Remove" />
+            <ReturnButton
+              className="ReturnButton"
+              onClick={handleReturnClicked}
+              onReset={handleReturnReset}
+            >
+              <img src={Return} alt="return" />
             </ReturnButton>
           </Stack>
         </Box>
-        </div>
-      </SliderContext.Provider>
-    )
+      </div>
+    </SliderContext.Provider>
+  );
 }
 export default ContinuousSlider;
--- src/Player.jsx
+++ src/Player.jsx
@@ -2,17 +2,30 @@ import * as React from "react";
 import ReactPlayer from "react-player";
 import { SliderContext } from "./ContinuousSlider";
 
-function Player() {
+function Player(props) {
+    const playerRef = React.useRef(null);
+    React.useEffect(() => {
+        console.log("effect");
+        if (props.returnClicked) {
+          console.log("clicked");
+          const currentTime = playerRef.current.getCurrentTime();
+          playerRef.current.seekTo(currentTime - 5);
+          props.onReturnReset();
+        }
+      }, [props.returnClicked]);
     const speed = React.useContext(SliderContext); 
+    const playbackRate = 0.25 + (speed * 0.015);
     return (
     <div className="playerWrapper">
-        <h1>{speed}</h1>
         <ReactPlayer 
         className="player" 
-        url={'https://www.youtube.com/watch?v=GDzzttUR7mM'}
+        url={'https://www.youtube.com/watch/VixkA3P-ULE'}
         width="100%"
         height="100%"
+        ref={playerRef}
         muted={true}
+        playing={true}
+        playbackRate={playbackRate}
         />
     </div>)
 }

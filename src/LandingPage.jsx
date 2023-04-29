a837939503d9f8653aff7365fd8257e4c4d4cb33
andreasprock <andreas_prock@hotmail.com>
2023-04-29 14:51:12 +0200

    LandingPage_Changes

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -1,13 +1,55 @@
 import React from "react";
 import { Link } from "react-router-dom";
-export default function HomePage() {
+import ToolBar from './ToolBar.jsx';
+import Content from './Content.jsx';
+import Cross from './svg/Cross.svg';
+import TUILogo from './svg/TUILogo.svg';
+import UnmazedLogo from './svg/Unmazed_LandingPage.svg';
+import UnmazedText from './svg/Unmazed_Text.svg';
+import LandingPage_GoButton from './svg/LandingPage_GoButton.svg'
+import { gsap } from 'gsap';
+
+
+
+
+
+
+
+
+export default function LandingPage() {
 return (
   <>
-   <h1>Hey from HomePage</h1>
-    <p>This is your awesome HomePage subtitle</p>
-    <p>
-        <Link to="/Navigation">Your desired Link</Link>
-    </p>
+   <div className='OutsideWrapper'>
+      <div className='AppWrapper'>
+        <div className='LandingWrapper_Outside'>
+
+          <div className='LandingWrapper_Logo'>
+
+            <img src={UnmazedLogo} className="LandingPage_UnmazedLogo"></img>
+            <img src={Cross} className="LandingPage_Cross"></img>
+            <img src={TUILogo} className="LandingPage_TUI"></img>
+
+          </div>
+
+          <div className="LandingWrapper_Text">
+
+            <img src={UnmazedText} className="LandingPage_UnmazedText"></img>
+            <p className="LandingPage_Text">
+              The definitive solution for indoor navigation
+            </p>
+
+          </div>
+
+          <a href="/Navigation" className="LandingPage_Circle">
+            <img src={LandingPage_GoButton} className="LandingPage_GoButton"></img>
+          </a>
+
+        </div>
+
+      </div>
+    </div> 
   </>
 );
 }
+
+gsap.from('.LandingPage_TUI', {duration: 1, y: '25%', x: '20%'});
